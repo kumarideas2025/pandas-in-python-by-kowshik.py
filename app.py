63863d1022a7a43a60af1e97823c7917dd967bc9
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2025-05-29 01:18:53 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+#all this for existened data file
+
+#read data from csv file into a datafile
+df = pd.read_csv("KUMAR.CSV")
+#read data from excel file into a datafile
+# df=pd.read_excel("business-operations-survey-2023-CSV-notes.xlsx",encoding="UTF8")
+
+
+#read data from json file into a datafile
+# df=pd.read_json("sample_data.json")
+s
+print(df)
+
+
+# gcsfs for could data
+
+ 
